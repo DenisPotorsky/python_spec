83f3e2312f9ed20e1db92a72b48989d89d6415fd
Den <denis-s2@yandex.ru>
2023-07-25 20:43:27 +0600

    homework completed

--- sem_3/camping_list.py
+++ sem_3/camping_list.py
@@ -6,10 +6,11 @@
 *Верните все возможные варианты комплектации рюкзака.
 """
 
-staff = {'Компас': 10, 'Палатка': 50, 'Спальный мешок': 40, 'Котелок': 20, 'Спички': 5,
-         'Крем от москитов': 5, 'Паёк': 30, 'Инструменты': 50, 'Плед': 15, 'Powerbank': 5}
+bucket_list = {'GPS навигатор': 150, 'Палатка': 3000, 'Спальный мешок': 500, 'Котелок': 2000, 'Спички': 5,
+               'Антимоскитный спрей': 50, 'Продукты': 5000, 'Инструменты': 4000, 'Матрас самонадувайка': 1500,
+               'Powerbank': 300}
 
-SIZE = 150
+SIZE = 15000
 
 
 def check_empty_space(sum_size, next_size):
@@ -21,21 +22,21 @@ def count_rest(sum_size):
 
 
 def collect_things():
-    temp_2 = {(value, key) for key, value in staff.items()}
+    temp_2 = {(value, key) for key, value in bucket_list.items()}
     temp = sorted(temp_2, reverse=True)
 
     for i in range(len(temp)):
         sum_size = 0
-        res_staff = {}
+        res_bucket = {}
         for j in range(i, len(temp)):
             if check_empty_space(sum_size, temp[j][0]):
-                res_staff.setdefault(temp[j][1], temp[j][0])
+                res_bucket.setdefault(temp[j][1], temp[j][0])
                 sum_size += temp[j][0]
         if i == 0:
-            print('"Жадный" вариант: ')
+            print('Допустимый вариант: ')
         elif i == 1:
-            print('Другие варианты: ')
-        print(res_staff)
+            print('Возможные варианты: ')
+        print(res_bucket)
 
 
 def get_key(d, value):
