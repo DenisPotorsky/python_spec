772f37123e8d22fe52afc2d59dc064ba88db64cb
DenisPotorsky <denis-s2@yandex.ru>
2023-09-19 08:50:18 +0500

    домашка всё. было весело. добавлен логгер и парсер.

--- sem_01/simple_nums.py
+++ sem_01/simple_nums.py
@@ -7,7 +7,7 @@ def prime(number):
     """
     INPUT_LIMIT = 100000
     while True:
-        #number = int(input('Введите число: '))
+        # number = int(input('Введите число: '))
         if number < 1 or number > INPUT_LIMIT:
             print('Введите положительное число до 100 000!')
             continue
@@ -18,8 +18,7 @@ def prime(number):
         return 'Простое число' if counter < 2 else 'Составное число'
 
 
-
-
-if 'name' == '__main__':
+if '__name__' == '__main__':
     import doctest
+
     doctest.testmod(verbose=True)
